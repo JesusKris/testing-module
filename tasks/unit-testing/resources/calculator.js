c13618a0eeb59a22b1cd0a5a17a209d49d5b83bf
Robert <robert.laur.private@gmail.com>
2023-06-09 22:13:51 +0300

    Added private methods to validate arguments in public methods

--- tasks/unit-testing/resources/calculator.js
+++ tasks/unit-testing/resources/calculator.js
@@ -1,12 +1,26 @@
 class Calculator {
- 
+
+    _isNum(num) {
+        return typeof num === "number" ? true : false
+    }
+
+    _validateInput(...numbers) {
+        numbers.forEach((num) => {
+            if (!this._isNum(num)) {
+                throw Error("Wrong data type as an argument. Only numbers are allowed!")
+            }
+        })
+    }
+
     /**
      * Adds two numbers
      * @param {number} num1
      * @param {number} num2
-     * @returns {number}
+     * @returns {number | error}
      */
     add(num1, num2) {
+        this._validateInput(num1, num2)
+
         return num1 + num2;
     }
 
@@ -14,9 +28,11 @@ class Calculator {
      * Subtracts two numbers
      * @param {number} num1
      * @param {number} num2
-     * @returns {number}
+     * @returns {number | error}
      */
     subtract(num1, num2) {
+        this._validateInput(num1, num2)
+
         return num1 - num2;
     }
 
@@ -24,9 +40,11 @@ class Calculator {
      * Multiplies two numbers
      * @param {number} num1
      * @param {number} num2
-     * @returns {number}
-     */
+     * @returns {number | error}
+    */
     multiply(num1, num2) {
+        this._validateInput(num1, num2)
+
         return num1 * num2;
     }
 
@@ -34,11 +52,13 @@ class Calculator {
      * Divides two numbers
      * @param {number} num1
      * @param {number} num2
-     * @returns {(number|error)}
-     */
+     * @returns {(number | error)}
+    */
     divide(num1, num2) {
+        this._validateInput(num1, num2)
+
         if (num2 === 0) {
-            throw new Error("Cannot divde by zero");
+            throw new Error("Cannot divde by zero!");
         }
 
         return num1 / num2;
@@ -48,9 +68,11 @@ class Calculator {
      * Remainder of division 
      * @param {number} num1
      * @param {number} num2
-     * @returns {number}
+     * @returns {number | error}
      */
     remainder(num1, num2) {
+        this._validateInput(num1, num2)
+
         return num1 % num2;
     }
 }
